80251f7b3d0724692054bbb6cfb03e746c6ffd98
James Dullaghan <james@fldlvl.com>
2015-05-01 18:26:55 -0700

    Add admin for question
    add flake8 and pep257 linter fixes

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,34 @@
+"""Django Models."""
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 class Question(models.Model):
-  question_text = models.CharField(max_length=200)
-  pub_date = models.DateTimeField('date_published')
+
+    """Question Class."""
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date_published')
+
+    def __str__(self):
+        """Human readable question text."""
+        return self.question_text
+
+    def was_published_recently(self):
+        """Method for recently published questions."""
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
-  question = models.ForeignKey(Question)
-  choice_text = models.CharField(max_length=200)
-  votes = models.IntegerField(default=0)
+
+    """Choice Class."""
+
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """Human readable choice text."""
+        return self.choice_text
